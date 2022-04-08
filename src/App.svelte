154c4074833496c13e538f9be5e109eb02a7392f
Van <53118297+bichvan2810@users.noreply.github.com>
2022-04-09 00:06:21 +0700

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+    import ChallengeSolvingModal from "./views/ChallengeModal.svelte";
+    import OuterDisplayBox from "./views/OuterDisplayBox.svelte";
+    import services from "./services";
+    import type PositionModel from '../commons/models/positionModel';
+
+    let isShowingChallenge: boolean = false;
+
+    services.getNewImageData();
+    
+    document.addEventListener("click", function(event: PointerEvent) {
+        for (let ele of event.composedPath()) {
+            if (ele.id === "ndtv-main-container") return;
+        }
+        isShowingChallenge = false;
+    });
+
+    function showChallengeModal() {
+        isShowingChallenge = true
+    }
+
+    
+
+</script>
+
+<main>
+    <div class="deleteable-content">
+        <p>ntbvavnvanerjvniaiieagineigen</p>
+        <p>ntbvavnvanerjvniaiieagineigen</p>
+    </div>
+    <div style="width: auto;">
+        <div id="ndtv-container">
+            <OuterDisplayBox on:showChallenge={showChallengeModal}/>
+            {#if isShowingChallenge}
+            <ChallengeSolvingModal/>
+            {/if}
+        </div>
+    </div>
+</main>
+
+<style>
+    #ndtv-container {
+        font-family: Arial !important;
+        color: white;
+        border: 1px solid pink;
+        border-radius: 2px;
+        width: 60%;
+        height: 4rem;
+        max-width: 300px;
+        overflow: visible;
+    }
+
+    :global(.mx-2) {
+        margin-left: 32px;
+        margin-right: 32px;
+    }
+
+    :global(.py-2) {
+        padding-bottom: 16px;
+        padding-top: 16px;
+    }
+
+    :global(.my-0) {
+        margin-bottom: 0px;
+        margin-top: 0px;
+    }
+</style>
---
+++ src/components/ChallengeModal/ChallengeModalContent.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+
+    import ChallengeModalImagesRow from "./ChallengeModalImagesRow.svelte";
+
+    let rowCount = 3, colCount = 3;
+
+</script>
+
+<div id="ndtv-content-container">
+    <div>
+        {#each {length: rowCount} as _, i}
+        <ChallengeModalImagesRow rowIndex={i} {colCount}/>
+        {/each}
+    </div>
+</div>
+
+<style>
+    #ndtv-content-container {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
